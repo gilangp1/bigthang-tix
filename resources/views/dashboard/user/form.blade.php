6b076feaa4ced9a197643a8a48bb850ee14cb486
gilangp1 <gilangprakoso37@gmail.com>
2020-11-25 01:21:51 +0700

    menambahkan tombol close pada modal user

--- resources/views/dashboard/user/form.blade.php
+++ resources/views/dashboard/user/form.blade.php
@@ -50,6 +50,7 @@
          <div class="modal-content">
             <div class="modal-header">
                <h5>Delete</h5>
+               <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
 
             <div class="modal-body">
