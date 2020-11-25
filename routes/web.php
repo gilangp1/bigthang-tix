7f7a73f73b24b2b1100702a8643b45d6078c7b7a
gilangp1 <gilangprakoso37@gmail.com>
2020-11-25 23:18:48 +0700

    Menambahkan middleware untuk halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,16 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/dashboard', 'Dashboard\DashboardController@index');
+Route::middleware('auth')->group(function (){
+    Route::get('/dashboard', 'Dashboard\DashboardController@index');
+    
+    //users
+    Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
+    Route::get('/dashboard/users/{id}', 'Dashboard\UserController@edit')->name('dashboard.users.edit');
+    Route::put('/dashboard/users/{id}', 'Dashboard\UserController@update')->name('dashboard.users.update');
+    Route::delete('/dashboard/users/{id}', 'Dashboard\UserController@destroy')->name('dashboard.users.delete');
+});
 
-//users
-Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
-Route::get('/dashboard/users/edit/{id}', 'Dashboard\UserController@edit')->name('dashboard.users.edit');
-Route::put('/dashboard/users/{id}', 'Dashboard\UserController@update')->name('dashboard.users.update');
-Route::delete('/dashboard/users/{id}', 'Dashboard\UserController@destroy')->name('dashboard.users.delete');
 
 
 
