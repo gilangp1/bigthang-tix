e1ea3293005cf2561003202b28a921fbe2758f8a
gilangp1 <gilangprakoso37@gmail.com>
2020-11-26 16:41:29 +0700

    penambahan route dan icon di dalam menu component

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,8 +1,10 @@
 <nav class="nav flex-column">
     <!-- The only way to do great work is to love what you do. - Steve Jobs -->
     @foreach ($list as $row)
-        <a class="nav-link {{ $isActive($row['label']) ? 'active' : '' }}" href="#">
-            {{ $row['label'] }}
+        <a class="nav-link {{ $isActive($row['label']) ? 'active' : '' }}" 
+           href=" {{ route($row['route']) }}">
+           <i class="icon-menu {{ $row['icon'] }}"></i>
+           {{ $row['label'] }}
         </a>
     @endforeach
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('auth')->group(function (){
-    Route::get('/dashboard', 'Dashboard\DashboardController@index');
-    
+
+    Route::get('/dashboard', 'Dashboard\DashboardController@index')->name('dashboard');
+
+    Route::get('/dashboard/movies', 'Dashboard\MoviesController@index')->name('dashboard.movies');
+    Route::get('/dashboard/theaters', 'Dashboard\TheatersController@index')->name('dashboard.theaters');
+    Route::get('/dashboard/tickets', 'Dashboard\TicketsController@index')->name('dashboard.tickets');
+
     //users
     Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
     Route::get('/dashboard/users/{id}', 'Dashboard\UserController@edit')->name('dashboard.users.edit');
