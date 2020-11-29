824da1574614073ba2f4b19137523b9f497665c5
gilangp1 <gilangprakoso37@gmail.com>
2020-11-29 23:58:57 +0700

    tampilkan thumbnail di dalam list php

--- resources/views/dashboard/movie/list.blade.php
+++ resources/views/dashboard/movie/list.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.dashboard')
 @section('content')
+
+   <div class="mb-2">
+      <a href="{{ route('dashboard.movies.create') }}" class="btn btn-primary btn-sm">+ Create</a>
+   </div>
+
    <div class="card">
       <div class="card-header">
          <div class="row">
@@ -25,18 +30,20 @@
          <table class="table table-borderless table-striped table-hover">
             <thead>
                <tr>
-                  <th>#</th>
-                  <th>Title</th>
                   <th>Thumbnail</th>
+                  <th>Title</th>
                   <th>&nbsp;</th>
                </tr>
             </thead>
             <tbody>
                @foreach ($movies as $movie)    
                <tr>
-                  <th scope="row">{{ ($movies->currentPage() - 1) * $movies->perPage() + $loop->iteration }} </th>
-                  <td>{{ $movie->title }}</td>
-                  <td>{{ $movie->thumbnail }}</td>
+                  <td class="col-thumbnail">
+                     <img src="{{ asset('storage/movies/'.$movie->thumbnail) }}" class="img-fluid">
+                  </td>
+                  <td>
+                     <h4><strong>{{ $movie->title }}</strong></h4>
+                  </td>
                   <td><a href="{{ route('dashboard.movies.edit', ['id' => $movie->id]) }}" class="btn btn-success btn-sm" title="edit">
                      <i class="fas fa-pen"></i></a>
                   </td>
