919368074cd832199be6d633c580c0277ec96165
gilangp1 <gilangprakoso37@gmail.com>
2020-12-07 19:24:34 +0700

    membuat form untuk module theater

---
+++ resources/views/dashboard/theater/form.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.dashboard')
+
+@section('content')
+   <div class="card">
+      <div class="card-header">
+         <div class="row">
+            <div class="col-8 align-self-center">
+               <h3>Theater</h3>
+            </div>
+
+            <div class="col-4 text-right">
+               <button class="btn btn-sm text-secondary" data-toggle="modal" data-target="#deleteModal">
+                  <i class="fas fa-trash"></i>
+               </button>
+            </div>
+         </div>
+      </div>
+
+      <div class="card-body">
+         <div class="row">
+            <div class="col-md-8 offset-md-2">
+               <form method="post" action="{{ route($url, $theater->id ?? '') }}" enctype="multipart/form-data">
+                  @csrf
+                  @if(isset($theater))
+                  @method('put')
+                  @endif
+                  <div class="form-group">
+                     <label for="theater">Theater</label>
+                     <input type="text" class="form-control @error('title') {{ 'is-invalid' }}  @enderror" name="theater" value="{{ old('theater') ?? $theater->title ?? '' }}">
+                     @error('title')
+                        <span class="text-danger">{{ $message }}</span>
+                     @enderror
+                  </div>
+                  <div class="form-group">
+                     <label for="theater">Address</label>
+                     <textarea name="theater" class="form-control @error('description') {{ 'is-invalid' }} @enderror">{{ old('description') ?? $theater->description ?? '' }}</textarea>
+                     @error('description')
+                        <span class="text-danger">{{ $message }}</span>
+                     @enderror
+                  </div>
+                  <div class="mb-4">
+                     <div class="form-group mt-4">
+                        <label for="status">Status</label>
+                     </div>
+                     <div class="form-check form-check-inline">
+                        <input type="radio" name="status" class="form-check-input" value="active" id="active">
+                        <label for="active" class="form-check-label">Active</label>
+                     </div>
+                     <div class="form-check form-check-inline">
+                        <input type="radio" name="status" class="form-check-input" value="inactive" id="inactive">
+                        <label for="inactive" class="form-check-label">Inactive</label>
+                     </div>
+                  </div>
+                  <div class="form-group mb-0">
+                     <button type="button" onclick="window.history.back();" class="btn btn-secondary btn-sm">Cancel</button>
+                     <button type="submit" class="btn btn-success btn-sm">{{ $button }}</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+
+   @if(isset($theater))
+   <div class="modal fade" id="deleteModal">
+      <div class="modal-dialog">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5>Delete</h5>
+               <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <div class="modal-body">
+               <p>Apakah anda yakin ingin menghapus film, <strong>{{ $theater->title }}</strong></p>
+            </div>
+
+            <div class="modal-footer">
+               <form action="{{ route('dashboard.movies.delete', $theater->id) }}" method="post">
+                  @csrf
+                  @method('delete')
+                  <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Delete</button>
+               </form>
+            </div>
+         </div>
+      </div>
+   </div>
+   @endif
+@endsection
