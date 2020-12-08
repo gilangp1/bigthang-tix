d4659f66754da38d22a61219a007a3ce4f9a0819
gilangp1 <gilangprakoso37@gmail.com>
2020-12-09 02:04:07 +0700

    menambahkan module pada messages store

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,7 +3,7 @@
 return [
 
     'store'     => 'Data :title berhasil ditambahkan',
-    'update'    => 'Data :title berhasil diganti',
+    'update'    => 'Data :btitle berhasil diganti menjadi :atitle',
     'delete'    => 'Data :title berhasil di hapus',
     'no_data'   => 'Belum ada data di module :module'
 ];
